e5c4b230292efab71664c61bc52b80cd40f51937
Rishabh Chhabra <rishabh.chhabra561@gmail.com>
2020-06-24 12:45:09 -0500

    Adds another path for testing and adds checking for key value map equality

--- watcher/handlerIngress_test.go
+++ watcher/handlerIngress_test.go
@@ -15,17 +15,33 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func TestBasic(t *testing.T) {
+func TestBasicAdd(t *testing.T) {
 	igHandler := createExampleIgHandler()
 	exampleIngress := createExampleIngress()
 
 	igHandler.add(&exampleIngress)
 
-	returnedKeys := igHandler.Ep.RedisClient.GetDBOneKeys()
+	returnedKeys := igHandler.Ep.RedisClient.GetDBOneKeyValues()
 
-	var expectedKeys []interface{}
-	expectedKeys = append(expectedKeys, "http://cafe.example.com/coffee")
-	expectedKeys = append(expectedKeys, "http://cafe.example.com/tea")
+	expectedKeys := getExpectedKeysForAdd()
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestBasicUpdate(t *testing.T) {
+	igHandler := createExampleIgHandler()
+	exampleIngress := createExampleIngress()
+	updatedExampleIngress := createExampleIngress()
+
+	updatedExampleIngress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[1].Path = "/app2-modified"
+
+	igHandler.update(&exampleIngress, &updatedExampleIngress)
+
+	returnedKeys := igHandler.Ep.RedisClient.GetDBOneKeyValues()
+
+	expectedKeys := getExpectedKeysForUpdate()
 
 	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
 		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
@@ -41,22 +57,38 @@ func createExampleIngress() v1beta1.Ingress {
 		Spec: v1beta1.IngressSpec{
 			Rules: []v1beta1.IngressRule{
 				{
-					Host: "cafe.example.com",
+					Host: "test.media.com",
 					IngressRuleValue: v1beta1.IngressRuleValue{
 						HTTP: &v1beta1.HTTPIngressRuleValue{
 							Paths: []v1beta1.HTTPIngressPath{
 								{
-									Path: "/coffee",
+									Path: "/app1",
 									Backend: v1beta1.IngressBackend{
-										ServiceName: "coffee-svc",
-										ServicePort: intstr.FromString("80"),
+										ServiceName: "appsvc1",
+										ServicePort: intstr.FromString("8080"),
 									},
 								},
 								{
-									Path: "/tea",
+									Path: "/app2",
 									Backend: v1beta1.IngressBackend{
-										ServiceName: "tea-svc",
-										ServicePort: intstr.FromString("80"),
+										ServiceName: "appsvc2",
+										ServicePort: intstr.FromString("8080"),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					Host: "test.edge.com",
+					IngressRuleValue: v1beta1.IngressRuleValue{
+						HTTP: &v1beta1.HTTPIngressRuleValue{
+							Paths: []v1beta1.HTTPIngressPath{
+								{
+									Path: "/app1",
+									Backend: v1beta1.IngressBackend{
+										ServiceName: "appsvc1",
+										ServicePort: intstr.FromString("8080"),
 									},
 								},
 							},
@@ -100,4 +132,28 @@ func createExampleEndpoint() ep.Endpoint {
 	}
 
 	return exampleEndpoint
+}
+
+func getExpectedKeysForUpdate() map[string][]string {
+	expectedKeys := getExpectedKeysForAdd()
+
+	delete(expectedKeys, "http://test.media.com/app2")
+
+	expectedKeys["http://test.media.com/app2-modified"] = []string{}
+	expectedKeys["http://test.media.com/app2-modified"] = append(expectedKeys["http://test.media.com/app2"], "trafficserver-test:appsvc2:8080")
+
+	return expectedKeys
+}
+
+func getExpectedKeysForAdd() map[string][]string {
+	expectedKeys := make(map[string][]string)
+	expectedKeys["http://test.edge.com/app1"] = []string{}
+	expectedKeys["http://test.media.com/app1"] = []string{}
+	expectedKeys["http://test.media.com/app2"] = []string{}
+
+	expectedKeys["http://test.edge.com/app1"] = append(expectedKeys["http://test.edge.com/app1"], "trafficserver-test:appsvc1:8080")
+	expectedKeys["http://test.media.com/app2"] = append(expectedKeys["http://test.media.com/app2"], "trafficserver-test:appsvc2:8080")
+	expectedKeys["http://test.media.com/app1"] = append(expectedKeys["http://test.media.com/app1"], "trafficserver-test:appsvc1:8080")
+
+	return expectedKeys
 }
