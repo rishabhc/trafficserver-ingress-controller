daa211d1191e04c76c26feb8fec662dadcf73ac8
Rishabh Chhabra <rishabh.chhabra561@gmail.com>
2020-06-29 13:02:30 -0500

    Adds tests for endpoint handler

--- watcher/handlerEndpoint_test.go
+++ watcher/handlerEndpoint_test.go
@@ -8,7 +8,7 @@ import (
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestAdd(t *testing.T) {
+func TestAdd_BasicEndpoint(t *testing.T) {
 	epHandler := createExampleEpHandler()
 	exampleV1Endpoint := createExampleV1Endpoint()
 
@@ -17,6 +17,164 @@ func TestAdd(t *testing.T) {
 	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
 
 	expectedKeys := getExpectedKeysForEndpointAdd()
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestAdd_IgnoreEndpointNamespace(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.Ep.NsManager.IgnoreNamespaceMap["trafficserver-test-2"] = true
+
+	epHandler.add(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := make(map[string][]string)
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_UpdateAddress(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.Subsets[0].Addresses[0].IP = "10.10.3.3"
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForAddressUpdate()
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_UpdatePortNumber(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.Subsets[0].Ports[0].Port = 8081
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForEndpointAdd()
+	expectedKeys["trafficserver-test-2:testsvc:8081"] = make([]string, 2)
+	expectedKeys["trafficserver-test-2:testsvc:8081"][0] = "10.10.2.2#8081#http"
+	expectedKeys["trafficserver-test-2:testsvc:8081"][1] = "10.10.1.1#8081#http"
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_UpdatePortName(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.Subsets[0].Ports[0].Name = "https"
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForEndpointAdd()
+	expectedKeys["trafficserver-test-2:testsvc:8080"] = make([]string, 2)
+	expectedKeys["trafficserver-test-2:testsvc:8080"][0] = "10.10.1.1#8080#https"
+	expectedKeys["trafficserver-test-2:testsvc:8080"][1] = "10.10.2.2#8080#https"
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_UpdateEndpointName(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.ObjectMeta.Name = "testsvc-modified"
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForEndpointAdd()
+	expectedKeys["trafficserver-test-2:testsvc-modified:8080"] = expectedKeys["trafficserver-test-2:testsvc:8080"]
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestDelete_DeleteEndpoint(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+	epHandler.delete(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := make(map[string][]string)
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_DeleteAddress(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.Subsets[0].Addresses = exampleV1Endpoint.Subsets[0].Addresses[:1]
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForEndpointAdd()
+	expectedKeys["trafficserver-test-2:testsvc:8080"] = expectedKeys["trafficserver-test-2:testsvc:8080"][:1]
+
+	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
+		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
+	}
+}
+
+func TestUpdate_AddAddress(t *testing.T) {
+	epHandler := createExampleEpHandler()
+	exampleV1Endpoint := createExampleV1Endpoint()
+
+	epHandler.add(&exampleV1Endpoint)
+
+	exampleV1Endpoint.Subsets[0].Addresses = append(exampleV1Endpoint.Subsets[0].Addresses, v1.EndpointAddress{
+		IP: "10.10.3.3",
+	})
+
+	epHandler.update(&exampleV1Endpoint)
+
+	returnedKeys := epHandler.Ep.RedisClient.GetDefaultDBKeyValues()
+
+	expectedKeys := getExpectedKeysForEndpointAdd()
+	expectedKeys["trafficserver-test-2:testsvc:8080"] = append(expectedKeys["trafficserver-test-2:testsvc:8080"], "10.10.3.3#8080#http")
 
 	if !reflect.DeepEqual(returnedKeys, expectedKeys) {
 		t.Errorf("returned \n%v,  but expected \n%v", returnedKeys, expectedKeys)
@@ -66,6 +224,20 @@ func getExpectedKeysForEndpointAdd() map[string][]string {
 
 	expectedKeys["trafficserver-test-2:testsvc:8080"] = append(expectedKeys["trafficserver-test-2:testsvc:8080"], "10.10.1.1#8080#http")
 	expectedKeys["trafficserver-test-2:testsvc:8080"] = append(expectedKeys["trafficserver-test-2:testsvc:8080"], "10.10.2.2#8080#http")
+
+	return expectedKeys
+}
+
+func getExpectedKeysForAddressUpdate() map[string][]string {
+	expectedKeys := getExpectedKeysForEndpointAdd()
+
+	if expectedKeys["trafficserver-test-2:testsvc:8080"][0] == "10.10.1.1#8080#http" {
+		expectedKeys["trafficserver-test-2:testsvc:8080"] = expectedKeys["trafficserver-test-2:testsvc:8080"][1:]
+	} else {
+		expectedKeys["trafficserver-test-2:testsvc:8080"] = expectedKeys["trafficserver-test-2:testsvc:8080"][:1]
+	}
+
+	expectedKeys["trafficserver-test-2:testsvc:8080"] = append(expectedKeys["trafficserver-test-2:testsvc:8080"], "10.10.3.3#8080#http")
 
 	return expectedKeys
 }
